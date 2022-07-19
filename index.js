56d263a22d015bebcf32b15e4b77d08b9cfe38fd
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-19 09:42:00 +0600

    Async pattern

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-console.log("Before"); // sync , blocking 
-setTimeout(() => {
-    console.log("Reading a user info from database.");
-    
-}, 2000); // Async , non-blocking 
-console.log("After"); // sync , blocking 
+console.log("Before"); 
+const user = getUser(1);
+console.log(user);
+console.log("After"); 
+
+function getUser ( id ){
+    setTimeout(() => {
+        console.log("Reading a user info from database.");
+        return {id: id, userName: "shubrato"}
+    }, 2000); 
+    return id;
+}
