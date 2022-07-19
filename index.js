3532330e8bc17281d331da811030352bcd26c644
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-19 11:07:34 +0600

    Replacing Callback to Promises

--- index.js
+++ index.js
@@ -15,21 +15,33 @@ function displayCommits(commits){
 
 
 // callback functions
-function getUser ( id, callback ){
-    setTimeout(() => {
-        console.log("Reading a user info from database.");
-        callback({id: id, userName: "shubrato"});
-    }, 2000); 
+function getUser ( id ){
+    return new Promise((resolve, reject)=>{
+        // Kick off some async work
+        setTimeout(() => {
+            console.log("Reading a user info from database.");
+            resolve({id: id, userName: "shubrato"});
+        }, 2000);
+    })
 }
 
-function getRepositories (username, callback){
-    setTimeout(() => {
-        console.log("Getting repositories...")
-        console.log("UserName : ", username)
-        callback(['repo1', 'repo2', 'repo3'])
-    }, 2000);
+function getRepositories (username){
+    return new Promise((resolve, reject)=>{
+        // Kick off some async work
+        setTimeout(() => {
+            console.log("Getting repositories...")
+            console.log("UserName : ", username)
+            resolve(['repo1', 'repo2', 'repo3'])
+        }, 2000);
+   })
 }
 
-function getCommits(repo, callback){
-
+function getCommits(repo){
+    return new Promise((resolve, reject)=>{
+        // Kick off some async work
+        setTimeout(() => {
+            console.log("Calling GitHub API...");
+            resolve(["commit"])
+        }, 2000);
+    })
 }
--- promise.js
+++ promise.js
@@ -5,10 +5,7 @@ const p = new Promise((resolve, reject)=>{
         // resolve(1);    // pending => resolved, fulfilled
         reject(new Error('There is an error!')); // pending => rejected
     }, 2000);
-    
-
 })
 
-p
-    .then(result => console.log('Result', result))
-    .catch(err => console.log('Error', err.message))
+p.then(result => console.log('Result', result))
+ .catch(err => console.log('Error', err.message))
