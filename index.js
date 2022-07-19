a19153fe03caa70aeaf6a49a0ceb976e4135afae
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-19 09:59:56 +0600

    callback done

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 console.log("Before"); 
-const user = getUser(1);
-console.log(user);
+getUser(1, (user)=> {
+    console.log("User", user);
+    // get the repos
+    getRepositories(user.userName, (repos)=>{
+        console.log('Repos : ', repos);
+    })
+});
 console.log("After"); 
 
-function getUser ( id ){
+// callback functions
+function getUser ( id, callback ){
     setTimeout(() => {
         console.log("Reading a user info from database.");
-        return {id: id, userName: "shubrato"}
+        callback({id: id, userName: "shubrato"});
     }, 2000); 
-    return id;
+}
+
+function getRepositories (username, repo){
+    setTimeout(() => {
+        console.log("Getting repositories...")
+        console.log("UserName : ", username)
+        repo(['repo1', 'repo2', 'repo3'])
+    }, 2000);
 }
