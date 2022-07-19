21c0eb0b6936420f832f464283226cbed18099f8
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-19 10:42:03 +0600

    Promises in async function

---
+++ promise.js
@@ -0,0 +1,14 @@
+const p = new Promise((resolve, reject)=>{
+    // Kick off some async work
+    // ...
+    setTimeout(() => {
+        // resolve(1);    // pending => resolved, fulfilled
+        reject(new Error('There is an error!')); // pending => rejected
+    }, 2000);
+    
+
+})
+
+p
+    .then(result => console.log('Result', result))
+    .catch(err => console.log('Error', err.message))
